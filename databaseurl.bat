9d9f3de21e9afc8795a17f79c3af25975641440f
rajhanshdhami <dhamirajhansh.007@gmail.com>
2023-04-23 12:46:04 +0530

    plain template for production

---
+++ databaseurl.bat
@@ -0,0 +1,1 @@
+set USE_DOCKER=n
---
+++ new/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from new.users.models import User
+from new.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
